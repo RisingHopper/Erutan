7e9d03dd2ee9d02d4390e255b9f93fd286f717b8
Joel Acapa <122485412+RisingHopper@users.noreply.github.com>
2023-04-10 23:20:09 +0200

    Add files via upload

---
+++ app/libs/Config.php
@@ -0,0 +1,18 @@
+<?php
+
+class Config {
+
+    public static $mvc_bd_hostname = "localhost";
+    public static $mvc_bd_nombre = "theErutan";
+    public static $mvc_bd_usuario = "root";
+    public static $mvc_bd_clave = "";
+
+    /*public static $mvc_bd_hostname = "127.0.0.1";
+    public static $mvc_bd_nombre = "sveiki";
+    public static $mvc_bd_usuario = "sveiki";
+    public static $mvc_bd_clave = "sveiki";*/
+    public static $mvc_vis_css = "style.css";
+    public static $vista = __DIR__ . '/../templates/inicio.php';
+    public static $menu = __DIR__ . '/../templates/menuInvitado.php';
+}
+?>
