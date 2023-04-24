99df3a4156a9e2591d7a0a5b15c89ed44a5bf058
Joel Acapa <122485412+RisingHopper@users.noreply.github.com>
2023-04-24 19:55:37 +0200

    Add files via upload

---
+++ web/components/menuAdmin.php
@@ -0,0 +1,35 @@
+<aside class=" col-3 g--font-size-22">
+
+  <div class="d-flex flex-column flex-shrink-0 p-3 bg-dark text-white" style="width: 340px; height:100vh; top:0; border-right:4px solid grey;">
+  <a class="navbar-brand mt-4" href="index.php?ctl=paginaDashboard"><img class="g--width-230" src="assets/img/logoWhite.png" alt="logo"></a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="index.php?ctl=paginaDashboard" class="nav-link text-white g--hover-lightskyblue-text">
+        <i class="fa-solid fa-chart-bar"></i>
+          Dashboard
+        </a>
+      </li>
+      <li>
+        <a href="index.php?ctl=paginaContenido" class="nav-link text-white g--hover-lightskyblue-text">
+        <i class="fa-solid fa-paintbrush"></i>
+          Content
+        </a>
+      </li>
+      <li>
+        <a href="index.php?ctl=paginaNewsletter" class="nav-link text-white g--hover-lightskyblue-text">
+        <i class="fa-regular fa-newspaper"></i>
+          Newsletter
+        </a>
+      </li>
+    </ul>
+    <hr>
+    <div class="px-3">
+      <a href="index.php?ctl=salir" class="nav-link text-white g--hover-lightskyblue-text">
+      <i class="fa-solid fa-door-open"></i>
+          Log out
+        </a>
+    </div>
+  </div>
+
+</aside>
---
+++ web/functions/getSessionLang.php
@@ -0,0 +1,4 @@
+<?php
+    session_start();
+    $lang = isset( $_SESSION['lang'] ) ? $_SESSION['lang'] : "es" ;
+?>
