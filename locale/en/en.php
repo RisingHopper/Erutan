5a80a944fd243628009fb135fada6396f087f248
Joel Acapa <122485412+RisingHopper@users.noreply.github.com>
2023-06-01 23:47:37 +0200

    Add files via upload

---
+++ locale/en/en.php
@@ -0,0 +1,179 @@
+<?php
+$lang = array(
+    'readMore' => "Leer Más",
+    //HOME
+    'home' => array(
+        "introduccion" => array(
+            'texto' => "El Erutan es un sitio donde encontrarás un montón de datos interesantes que probablemente no sabías hasta ahora. Sé que leer es muy aburrido, por eso me he asegurado de que tu experiencia en este sitio sea más agradable.",
+        ),
+        "entradas" => array(
+            'titulo' => "Entradas",
+        ),
+    ),
+     /*submenu de Informacion */
+    'about' => array(
+        "title" => array(
+            'titulo' => "Sobre Erutan",
+        ),
+        "texto" => array(
+            'parrafo' => "Erutan es un sitio web creado con el propósito de entretener a las personas con información sobre la naturaleza y los animales. Los usuarios de este sitio web para encontrar su plato favorito. Varios restaurantes están registrados en este sitio, puede visitarlos y dejar un comentario para ayudar a otras personas a hacer su elección. El concepto es muy simple, debe elegir su categoría favorita, luego el sitio mostrará todos los restaurantes que tienen esa categoría que ha elegido. Después de eso, podrá ver los platos específicos que están en su categoría.",
+        ),
+        "team" => array(
+            'equipo' => "Equipo Erutan",
+        ),
+    ),
+    // NEWSLETTER
+    'newsletter'=>array(
+        'title'=>" Suscribete a nuestra newsletter",
+        'texto'=>"Únase a nuestra lista de suscriptores para recibir las últimas noticias, actualizaciones y ofertas especiales directamente en su bandeja de entrada.",
+        'btn'=>"Enviar",
+        'name'=>"Nombre",
+        'tlf'=>"Telefono",
+        'email'=>"Correo eléctronico",
+    ),
+
+    // INDEX
+    'index' =>array(
+
+        "textos" => array(
+            'titulo' => "Destino Rías Baixas",
+            'cuerpo' => "El SonRías Baixas se celebra en Bueu, en la Península do Morrazo, un <b>paraíso atlántico ubicado en plenas Rías Baixas</b> con una costa salvaje e infinitas playas que la hacen única en el mundo. O Morrazo es un destino turístico atractivo y moderno en cualquier época del año. Un lugar de obligada visita que vamos a desgranar al por menor para seáis partícipes de sus innumerables encantos y atractivos. <br><br>
+
+            ¡Las mejores <b>playas, gastronomía con producto local, rutas de senderismo y varias opciones de turismo</b> en la zona es lo que queremos compartir con vosotros para que vuestra <b>experiencia festivalera</b> sea <b>inolvidable</b>!",
+            'otro' => "#RÍASBAIXASFEST"
+        ),
+        "counter"=>array(
+            "titulo"=>"Entradas a la venta en:",
+            "fecha"=>"ENTRADAS A LA VENTA MARTES 21 A LAS 12h00"
+        ),
+        "entradas" => "Entradas",
+        "newsletter"=> "Suscríbete a nuestra newsletter"
+    ),
+
+
+
+    /*Destinos */
+    'miradores' => array(
+        "facho" => array(
+            'titulo' => "FACHO DE DONÓN",
+            'descripcion'  => "Un camino de piedra sube lentamente entre petroglifos y aldeas abandonadas hace siglos, hasta un mirador que vigila la eterna puesta del sol. Es el <b>Facho de Donón</b>, un lugar al que peregrinaron los primeros pobladores de la comarca en busca de salud y buena fortuna. <br><br>
+            Es difícil no imaginarlos con sus antorchas encendidas en la mano mientras pisaban por donde hoy lo hacemos nosotros, hasta ver uno de los paisajes más impactante de las Rías Baixas. A sus pies, la escarpada costa de la vela, que <b>parece casi tocar las islas Cíes mientras entra en el Atlántico</b>. <br><br>            
+            Este punto, el Monte Facho, es uno de los <b>grandes miradores naturales de O Morrazo</b>, pero no es el único. Ni tampoco es el punto más alto desde el que contemplar sus costas y valles. Aquí, los miradores imprescindibles del Morrazo.",
+            'direccion'=> "<i>Facho de Donón: GPS 42.276291, -8.861236 | Dirección: Garita de Donón, Donón, O Hío (Santo André), Cangas</i>",
+            'direccion2' => "",
+            'img1'=>"",
+            'alt1'=>"",
+            'img2'=>"",
+            'alt2'=>"",
+            'img3'=>"",
+            'alt3'=>"",
+        ),
+    ),
+    'playas' => array(
+        "salvajes" => array(
+            'titulo' => "PLAYAS SALVAJES: EL DESCANSO MÁS NATURAL",
+            'descripcion'  => "
+            Al amparo de los <b>espacios protegidos de Cabo Udra, la Costa da Vela y el Parque Nacional das Illas Atlánticas,</b> se encuentran un gran número de <b>playas y calas de carácter salvaje en los que reconectar con la naturaleza y redescubrir lo son del mar.</b> <br><br>
+            
+            Entre las favoritas de los bañistas está la playa de <b>Barra</b>, un icono de las playas naturistas de Galicia, situada lejos de los grandes núcleos de población y protegida por un sistema dunar y un gran pinar. <br><br>
+            
+            La punta <b>Mexilloeira, separa esta magnífica playa de las de Viñó y la de Nerga,</b> ambas de arena fina y en un ambiente natural, ideales para tomar el sol y pasear. Las tres forman parte del <b>Complejo Dunar Nerga-Barra,</b> aunque la de Nerga además ejerce el reconocimiento de <b>Bandera Azul</b>, lo que la dota de mayores servicios a lo largo del año.",
+            'direccion'=> "",
+            'direccion2' => "",
+   
+        ),
+        "banderaAzul" => array(
+            'titulo' => "PLAYAS DE BANDERA AZUL: GARANTÍA DE CALIDAD",
+            'descripcion'  => "El reconocimiento de la Fundación Europea de Educación Ambiental a través de la Bandera Azul, ve en el Morrazo un paso más allá: la <b>región cuenta no solo con 12 playas de bandera azul, sino con tres senderos azules (Tirán, Cabo Udra y Donón) y un centro interpretativo (Cabo Udra)</b> con este reconocimiento. Forma parte del <b>compromiso ambiental de la región con la conservación del entorno, la seguridad y los servicios</b>, completando así la protección otorgada por la Red Natura 2000 y el amparo del Parque Nacional das Illas Atlánticas. <br><br>
+
+            En el caso de las playas, esta bandera <b>garantiza la calidad de las aguas para el baño, la información y educación ambiental</b> para los usuarios de las mismas, una adecuada <b>gestión ambiental e instalaciones, servicios y seguridad</b> en la temporada alta de baño. ",
+            'direccion'=> "",
+            'direccion2' => "",
+            'img1'=>"",
+            'alt1'=>"",
+            'img2'=>"",
+            'alt2'=>"",
+            'img3'=>"",
+            'alt3'=>"",
+        ),
+        "urbanas" => array(
+            'titulo' => "PLAYAS URBANAS Y SEMIURBANAS",
+            'descripcion'  => "Para aquellos viajeros que buscan un <b>acceso más sencillo</b> en el que disfrutar del sol y del mar en cuanto exploran O Morrazo, las <b>playas de Rodeira, en Cangas, y Xunqueira, en Moaña</b>, ofrecen un entorno muy cuidado al lado de todos los servicios y con unas vistas envidiables de la ría de Vigo. <br><br>
+
+            También en Moaña, la <b>pequeña playa del Con</b>, más alla de ser bandera azul, se encuentra resguardada por el muelle y ofrece otra perspectiva interesante sobre Vigo y su ría. ",
+            'direccion'=> "",
+            'direccion2' => "",
+            'img1'=>"",
+            'alt1'=>"",
+            'img2'=>"",
+            'alt2'=>"",
+            'img3'=>"",
+            'alt3'=>"",
+        ),
+        "seccion" => array(
+            'titulo' => "Playas",
+            'tituloSeccion' => "Playas"
+        ),
+    ),
+
+ /*FAQS*/
+ 'collapseFaqs' => array(
+    "entradas" => array(
+        'titulo' => "Entradas",
+        'cuerpo'  => "
+                    <p><b>Compré mi entrada para un solo día y ahora quiero ir los tres, ¿qué puedo hacer?</b>  No te preocupes, puedes hacer un upgrade de tu entrada tu misma desde este enlace <a class='g--color-darkBlue' href='https://venta.enterticket.es/cambios/entradas'><u>https://venta.enterticket.es/cambios/entradas</u></a>
+                    </p>
+                    <p><b>¿Puede usar la entrada una persona distinta de la que la compró?</b> Sí, todo el mundo tiene la opción de un cambio de nombre por entrada, que puede hacerse en el siguiente enlace: <a class='g--color-darkBlue' href='https://venta.enterticket.es/cambios'><u>https://venta.enterticket.es/cambios</u></a> Recuerda que, aunque cambie la titularidad de la entrada, todas las comunicaciones del festival se realizarán al comprador/a de la misma.  
+                    </p>
+                    <p><b>¿Tienes la entrada de 2020?</b> Tu entrada es válida para 2022 automáticamente, no es necesario que hagas nada. 
+                    </p>
+                    <p><b>En la taquilla ¿subirán los precios de las entradas?</b> Sí. Costará 70 euros el abono. Las entradas de un sólo día costarán 35 euros. 
+                    </p>
+                    <p><b>¿Y hasta cuando se venden las entradas anticipadas?</b> Venta de entradas online durante el festival (tened en cuenta que en la taquillan siguen en venta durante todo el evento):
+                    <ul>
+                    <li><p>Entrada de un día para el jueves: el propio jueves hasta las 11h45. </p></li>
+                    <li><p>Entrada de un día para el viernes: el propio viernes hasta las 11h45. </p></li>
+                    <li><p>Entrada de un día para el sábado: el propio sábado hasta las 11h00. </p></li>
+                    <li><p>Abonos: hasta el jueves a las 11h45. </p></li>
+                    </ul>
+                    </p>
+                    <p><b>¿Dispongo de descuentos por estar en posesión del CARNÉ XOVE de la Xunta de Galicia?*</b> Sí, indicando con la compra el código: CARNEXOVEFESTIVAIS. Este código es de aplicación individual y solamente puede usarse a título personal. Es decir, no es una promoción, es un código de descuento que se aplica si la persona que compra acredita estar en posesión del Carné Xove. Tampoco es un descuento aplicado a grupos, solamente se puede beneficiar cada persona individualmente. <b><u><i>CUPO AGOTADO*</i></u></b></p>
+                    <p><b>Compré la entrada por internet, ¿tengo que llevarla impresa?</b> No, puedes traerla descargada en el móvil tanto como pdf como en el wallet. <b>¿Y se llevo el DNI?</b> No podremos dejarte pasar solamente con el DNI, tienes que enseñarnos también el código de la entrada. </p>",
+    ),
+  
+   
+    "aparcamientos" => array(
+        'titulo' => "Aparcamientos",
+        'cuerpo'  => "
+                    <p><b>¿Hay parking para furgos y caravanas?</b> No, pero se recomienda aparcar en: <a class='g--color-darkBlue' href='https://www.google.es/maps/place/R%C3%BAa+de+Alexandre+B%C3%B3v%C3%AAda,+83A,+36937+Bueu,+Pontevedra/@42.3241144,-8.790549,166m/data=!3m2!1e3!4b1!4m6!3m5!1s0xd2f66fdc6c6c5fd:0xefc48870786c6c52!8m2!3d42.3241144!4d-8.790549!16s%2Fg%2F11c1yyd2gl?hl=es'><u>esta explanada</u></a> que linda con la zona de acampada en la zona del Pabellón Municipal de Bueu: <a class='g--color-darkBlue' href='https://www.google.es/maps/place/42%C2%B019'11.2%22N+8%C2%B047'14.7%22W/@42.319767,-8.7885193,307m/data=!3m2!1e3!4b1!4m6!3m5!1s0x0:0x528c77b9bc7f7043!7e2!8m2!3d42.319765!4d-8.7874253?hl=es&shorturl=1'><u>zona 1</u></a> o <a class='g--color-darkBlue' href='https://www.google.es/maps/place/Parking/@42.319767,-8.7885193,307m/data=!3m1!1e3!4m5!3m4!1s0xd2f679e8a472141:0x39b53be3e3c47ba!8m2!3d42.3193662!4d-8.7863313?hl=es&shorturl=1'><u>zona 2</u> </a> o en el <a class='g--color-darkBlue' href='https://www.google.es/maps/place/42%C2%B019'44.9%22N+8%C2%B047'13.8%22W/@42.329134,-8.788833,613m/data=!3m2!1e3!4b1!4m6!3m5!1s0x0:0x46280ba792019c41!7e2!8m2!3d42.3291305!4d-8.7871559?hl=es&shorturl=1'><u>puerto</u></a>. 
+                    </p>
+                    <p><b>¿Y si voy en coche? </b>
+                    <ul>
+                    <li><a class='g--color-darkBlue' href='https://www.google.es/maps/place/R%C3%BAa+de+Alexandre+B%C3%B3v%C3%AAda,+83A,+36937+Bueu,+Pontevedra/@42.3241154,-8.7910962,153m/data=!3m2!1e3!4b1!4m5!3m4!1s0xd2f66fdc6c6c5fd:0xefc48870786c6c52!8m2!3d42.3241144!4d-8.790549?hl=es&shorturl=1'><u>Cerca de la zona de acampada</u></a></p></li>
+                    <li><a class='g--color-darkBlue' href='https://www.google.es/maps/place/42%C2%B019'11.2%22N+8%C2%B047'14.7%22W/@42.319767,-8.7885193,307m/data=!3m2!1e3!4b1!4m6!3m5!1s0x0:0x528c77b9bc7f7043!7e2!8m2!3d42.319765!4d-8.7874253?hl=es&shorturl=1'><u>Zona del Pabellón Municipal</u></a></p></li>
+                    <li><a class='g--color-darkBlue' href='https://www.google.es/maps/place/Parking/@42.319767,-8.7885193,307m/data=!3m1!1e3!4m5!3m4!1s0xd2f679e8a472141:0x39b53be3e3c47ba!8m2!3d42.3193662!4d-8.7863313?hl=es&shorturl=1'><u>Aparcamiento A Pedra</u></a></p></li>
+                    <li><a class='g--color-darkBlue' href='https://www.google.es/maps/place/Aparcamiento/@42.325036,-8.785551,161m/data=!3m1!1e3!4m5!3m4!1s0xd2f67d5e1bd58f7:0xd3c49a20195d68a0!8m2!3d42.3251755!4d-8.7855974?hl=es&shorturl=1'><u>Aparcamiento Santán</u></a></p></li>
+                    <li><a class='g--color-darkBlue' href='https://www.google.es/maps/place/Aparcamiento/@42.3247342,-8.7855256,161m/data=!3m1!1e3!4m5!3m4!1s0xd2f677bec4c4d77:0xb6cb6350dbd63f04!8m2!3d42.3245947!4d-8.7852172?hl=es&shorturl=1'><u>Aparcamiento Jose María Estévez</u></a></p></li>
+                    <li><a class='g--color-darkBlue' href='https://www.google.es/maps/place/Aparcamiento/@42.3278087,-8.7813616,266m/data=!3m1!1e3!4m5!3m4!1s0xd2f67b41898e577:0xca6d2852dd54f196!8m2!3d42.3280102!4d-8.78063?hl=es&shorturl=1'><u>Aparcamiento Montero Ríos</u></a></p></li>
+                    <li><a class='g--color-darkBlue' href='https://www.google.es/maps/place/42%C2%B019'44.9%22N+8%C2%B047'13.8%22W/@42.329134,-8.788833,613m/data=!3m2!1e3!4b1!4m6!3m5!1s0x0:0x46280ba792019c41!7e2!8m2!3d42.3291305!4d-8.7871559?hl=es&shorturl=1'><u>Puerto</u></a></p></li>
+                    </ul>
+                    </p>",
+    ),
+    "accesoDeMenores" => array(
+        'titulo' => "Acceso de menores",
+        'cuerpo'  => "
+                    <p><b>Puedo ir a los conciertos de la noche se tengo menos de 18 años?</b> Sí. Las y los menores de 0 a 12 años (incluidas) accederán al recinto de manera gratuita y las y los menores de 13 a 17 años (ambas edades incluidas) deberán adquirir una entrada de persona adulta.
+                    </p>
+                    <p><b>¿Necesitarán entrada para acceder al recinto los menores de 12 años?</b> Sí, necesitarán una entrada que podréis <a class='g--color-darkBlue' href=''><u>descargar aquí de manera gratuita</u></a><b> o solicitar en la propia taquilla.</b> Una vez en la taquilla del festival, se canjeará por una pulsera para acceder al recinto de los conciertos. El/la menor tendrá que estar en <b>compañía de un progenitor o tutor/a en el recinto en todo momento.</b> Al acceder se le podrá requerir a los progenitores o tutores que muestren el documento original de su DNI y el DNI del menor y firmar un documento de autorización y de aceptación de responsabilidad que pueden descargar previamente <a class='g--color-darkBlue' href='https://playplan.sharepoint.com/sites/srbcomunicacion/Documentos%20compartidos/Forms/AllItems.aspx?id=%2Fsites%2Fsrbcomunicacion%2FDocumentos%20compartidos%2FSRB%202022%2FDocumentaci%C3%B3n%20SRB2022%2FAutorizaci%C3%B3n%20menores%2FAutorizacionMenores%5FGAL%20SRB2022%2Epdf&parent=%2Fsites%2Fsrbcomunicacion%2FDocumentos%20compartidos%2FSRB%202022%2FDocumentaci%C3%B3n%20SRB2022%2FAutorizaci%C3%B3n%20menores&p=true&ga=1'><u>aquí</u></a>. Si el acompañante no es un tutor/a legal, deberá mostrar también una fotocopia del DNI de un progenitor o tutor/a del menor, acercar la autorización de asistencia firmada por parte de alguno de los anteriormente citados y enseñar el DNI del menor.
+                    </p>
+                    <p><b>¿Y puedo ir solo o tengo que ir acompañado de un adulto? Todo menor de 16 años de edad  que vaya a acceder debe ir acompañado de un progenitor o tutor/a.</b> Al acceder al recinto de conciertos se le podrá requerir a los progenitores o tutores que muestren el documento original de su DNI y el DNI del menor, dar un número de teléfono de contacto y firmar un documento de autorización y de aceptación de responsabilidad que pueden descargar previamente <a class='g--color-darkBlue' href='https://playplan.sharepoint.com/sites/srbcomunicacion/Documentos%20compartidos/Forms/AllItems.aspx?id=%2Fsites%2Fsrbcomunicacion%2FDocumentos%20compartidos%2FSRB%202022%2FDocumentaci%C3%B3n%20SRB2022%2FAutorizaci%C3%B3n%20menores%2FAutorizacionMenores%5FGAL%20SRB2022%2Epdf&parent=%2Fsites%2Fsrbcomunicacion%2FDocumentos%20compartidos%2FSRB%202022%2FDocumentaci%C3%B3n%20SRB2022%2FAutorizaci%C3%B3n%20menores&p=true&ga=1'><u>aquí</u></a>. Si el acompañante no es un tutor/a legal, deberá mostrar también una fotocopia del DNI de un progenitor o tutor/a del menor, acercar la autorización de asistencia firmada por parte de alguno de los anteriormente citados y enseñar el DNI del menor.   
+                    </p>
+                    <p><b>Los/las menores de entre 16 y 17 años pueden acceder al recinto de conciertos sin la compañía de un adulto</b> pero deben enseñar igualmente, antes de acceder al recinto, su DNI, una fotocopia del de su progenitor o tutor y una autorización firmada por este último. En la autorización, de la que podéis descargar un modelo <a class='g--color-darkBlue' href='https://playplan.sharepoint.com/sites/srbcomunicacion/Documentos%20compartidos/Forms/AllItems.aspx?id=%2Fsites%2Fsrbcomunicacion%2FDocumentos%20compartidos%2FSRB%202022%2FDocumentaci%C3%B3n%20SRB2022%2FAutorizaci%C3%B3n%20menores%2FAutorizacionMenores%5FGAL%20SRB2022%2Epdf&parent=%2Fsites%2Fsrbcomunicacion%2FDocumentos%20compartidos%2FSRB%202022%2FDocumentaci%C3%B3n%20SRB2022%2FAutorizaci%C3%B3n%20menores&p=true&ga=1'><u>aquí</u></a>, el progenitor o tutor autoriza la entrada al recinto de conciertos.
+                    </p> 
+                    <p><b>Y los niños pequeños, ¿pueden entrar a los conciertos?</b> Aunque no es un festival pensado para niños y niñas, pueden, sí. Si son menores de 12 años, además, no pagarán entrada.         
+                    </p>",
+    )
+)
+)
+?>
+
