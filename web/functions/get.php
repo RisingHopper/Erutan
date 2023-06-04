80f1c0336e1336a1bfd7d78f27f911d9c6a9f956
Joel Acapa <122485412+RisingHopper@users.noreply.github.com>
2023-06-04 23:03:07 +0200

    Delete web directory

--- web/functions/get.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once ("inc-dbconnect.php");
-    $dataBanner = $db->prepare("SELECT * FROM bbdd");
-    $dataBanner->execute();
-    $dataBanner = $dataBanner->fetchAll(PDO::FETCH_ASSOC);
-?>
